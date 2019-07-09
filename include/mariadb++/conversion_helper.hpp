b23e15a2fdf16b7d4cadf1c549e6f3057f6d2456
Dedmen Miller <dedmen@dedmen.de>
2019-07-09 15:02:48 +0200

    Linux build fix

--- include/mariadb++/conversion_helper.hpp
+++ include/mariadb++/conversion_helper.hpp
@@ -12,36 +12,55 @@
 #include <limits>
 #include <stdexcept>
 #include <charconv>
+#include <utility>
 
 #ifdef WIN32
 #undef max
 #undef min
 #endif
 
+#if _MSVC_LANG >= 201703 && !defined(__cpp_lib_to_chars)
+#define __cpp_lib_to_chars
+#endif
+
 template <typename T, typename K>
 inline T checked_cast(K value) {
-    if (value < std::numeric_limits<T>::lowest() || value > std::numeric_limits<T>::max()) return T();
+    if (value < std::numeric_limits<T>::lowest() || value > std::numeric_limits<T>::max())
+        return T();
 
     return static_cast<T>(value);
 }
 
 template <typename T>
 inline T string_cast(const intercept::types::r_string &str) {
+#ifdef __cpp_lib_to_chars
     int parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (p != str.data()+str.size()) return T();
+    if (p != str.data() + str.size()) return T();
 
     return checked_cast<T>(parsedNumber);
+#else
+    int parsedNumber = std::atoi(str.c_str());
+
+    return checked_cast<T>(parsedNumber);
+#endif
 }
 
 template <>
 inline unsigned long string_cast(const intercept::types::r_string &str) {
+#ifdef __cpp_lib_to_chars
     unsigned long parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (p != str.data()+str.size()) return 0;
+    if (p != str.data() + str.size()) return 0;
+#else
+    char *endPos;
+    unsigned long parsedNumber = std::strtoul(str.c_str(), &endPos, 10);
 
+    if (endPos != str.end().operator const char *()) return 0;
+
+#endif
     return parsedNumber;
 }
 
@@ -55,50 +74,78 @@ inline unsigned int string_cast(const intercept::types::r_string &str) {
 template <>
 inline unsigned long long string_cast(const intercept::types::r_string &str) {
     unsigned long long parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+#ifdef __cpp_lib_to_chars
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (p != str.data()+str.size()) return 0;
+    if (p != str.data() + str.size()) return 0;
+#else
+    char *endPos;
+    parsedNumber = std::strtoull(str.c_str(), &endPos, 10);
 
+    if (endPos != str.end().operator const char *()) return 0;
+
+#endif
     return parsedNumber;
 }
 
 template <>
 inline long long string_cast(const intercept::types::r_string &str) {
     long long parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+#ifdef __cpp_lib_to_chars
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (p != str.data()+str.size()) return 0;
+    if (p != str.data() + str.size()) return 0;
+#else
+    char *endPos;
+    parsedNumber = std::strtoll(str.c_str(), &endPos, 10);
 
+    if (endPos != str.end().operator const char *()) return 0;
+
+#endif
     return parsedNumber;
 }
 
 template <>
 inline double string_cast(const intercept::types::r_string &str) {
     double parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+#ifdef __cpp_lib_to_chars
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (ec ==  std::errc::result_out_of_range) {
+    if (ec == std::errc::result_out_of_range) {
         // Not a Number double
         return std::numeric_limits<double>::quiet_NaN();
     }
 
-    if (p != str.data()+str.size()) return 0;
+    if (p != str.data() + str.size()) return 0;
+#else
+    char *endPos;
+    parsedNumber = std::strtod(str.c_str(), &endPos);
 
+    if (endPos != str.end().operator const char *()) return 0;
+
+#endif
     return parsedNumber;
 }
 
 template <>
 inline float string_cast(const intercept::types::r_string &str) {
     float parsedNumber;
-    auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), parsedNumber);
+#ifdef __cpp_lib_to_chars
+    auto [p, ec] = std::from_chars(str.data(), str.data() + str.size(), parsedNumber);
 
-    if (ec ==  std::errc::result_out_of_range) {
+    if (ec == std::errc::result_out_of_range) {
         // Not a Number float
         return std::numeric_limits<float>::quiet_NaN();
     }
 
-    if (p != str.data()+str.size()) return 0;
+    if (p != str.data() + str.size()) return 0;
+#else
+    char *endPos;
+    parsedNumber = std::strtof(str.c_str(), &endPos);
 
+    if (endPos != str.end().operator const char *()) return 0;
+
+#endif
     return parsedNumber;
 }
 
